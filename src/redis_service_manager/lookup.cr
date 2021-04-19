b032e7e423e9796460b5f52a71e23376df4e65d4
Stephen von Takach <steve@vontaka.ch>
2021-04-19 10:16:03 +1000

    feat: minor performance increase for lookup

--- src/redis_service_manager/lookup.cr
+++ src/redis_service_manager/lookup.cr
@@ -5,7 +5,7 @@ require "rendezvous-hash"
 class RedisServiceManager
   class Lookup
     def initialize(service : String, @ttl : Time::Span = 5.seconds)
-      @nodes = [] of String
+      @nodes = RendezvousHash.new
       @expires = Time.unix(0)
       @hash_key = "service_#{service}_lookup"
     end
@@ -14,13 +14,12 @@ class RedisServiceManager
     # single redis client in use for services that just need to discover which
     # core a module is running on
     def nodes(redis : Redis::Client, fresh : Bool = false)
-      return RendezvousHash.new(nodes: @nodes) unless fresh || Time.utc >= @expires
+      return @nodes unless fresh || Time.utc >= @expires
 
       hash = NodeHash.new(@hash_key, redis).to_h
       keys = hash.keys.sort!
       @expires = @ttl.from_now
-      @nodes = keys.map { |key| hash[key] }
-      RendezvousHash.new(nodes: @nodes)
+      @nodes = RendezvousHash.new(nodes: keys.map { |key| hash[key] })
     end
   end
 end
